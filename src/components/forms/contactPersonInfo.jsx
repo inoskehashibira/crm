d4e90dbbcd0934a37ad49113d17920dcda820a5b
inoskehashibira <shakhawat.iiuc.se@gmail.com>
2022-12-12 12:26:20 +0600

    cp works fine

--- src/components/forms/contactPersonInfo.jsx
+++ src/components/forms/contactPersonInfo.jsx
@@ -36,17 +36,17 @@ export default function ContactPersonInfo() {
 
         <form className="row g-3" onSubmit={handleSubmit(onSubmit)}>
 
-          
+
 
           <div className="col-md-6">
             <label htmlFor="firstName" className="form-label">
-              Customer Name
+              Contact Name
             </label>
             <input
               // {...register("Customer_Name")}
               type="text"
               className="form-control"
-              id="Customer_Name"
+              id="Contact_Name"
               required
               onChange={handleChange}
             // value = "abc"
@@ -55,46 +55,34 @@ export default function ContactPersonInfo() {
 
           <div className="col-md-6">
             <label htmlFor="firstName" className="form-label">
-              Organization
+              Job Position
             </label>
             <input
               type="text"
               // {...register("Organization")}
               className="form-control"
-              id="Organization"
+              id="cp_Job_position"
               required
               onChange={handleChange}
             ></input>
           </div>
 
           <div className="col-md-6">
-            <label htmlFor="emailinfo" className="form-label">
-              E-mail
+            <label htmlFor="Address" className="form-label">
+              Address
             </label>
             <input
-              type="email"
-              // {...register("email")}
+              type="text"
+              // {...register("Organization")}
               className="form-control"
-              id="emailinfo"
+              id="cp_Address"
               required
               onChange={handleChange}
             ></input>
           </div>
 
-          <div className="col-md-4">
-            <label htmlFor="Phone_Number" className="form-label">
-              Phone Number
-            </label>
-            <input
-              type="text"
-              // {...register("Phone_Number")}
-              className="form-control"
-              id="Phone_Number"
-              placeholder="+88"
-              required
-              onChange={handleChange}
-            ></input>
-          </div>
+
+
 
           {/* <div>
             <RBcomp
@@ -104,65 +92,122 @@ export default function ContactPersonInfo() {
           </div> */}
 
 
-          <div className="col-md-4">
+          <div className="row mt-1 mb-1" >
+            <div className="col">
+              <label htmlFor="Address" className="form-label">
+                City
+              </label>
+              <select className="form-select " onChange={handleChange} id="city">
+                <option value="Dhaka">
+                  Dhaka
+                </option>
+
+                <option value="Chittagong">
+                  Chittagong
+                </option>
+
+                <option value="Syhlet">
+                  Syhlet
+                </option>
+              </select>
+            </div>
+
+
+            <div className='col'>
+              <label htmlFor="Address" className="form-label">
+                Country
+              </label>
+              <select className="form-select" onChange={handleChange} id="country">
+                <option value="Bangladesh">
+                  Bangladesh
+                </option>
+
+                <option value="Nepal">
+                  Nepal
+                </option>
+
+                <option value="Pakistan">
+                  Pakistan
+                </option>
+              </select>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col">
+
+              <div className="col-md-6">
+                <label htmlFor="emailinfo" className="form-label">
+                  E-mail
+                </label>
+                <input
+                  type="email"
+                  // {...register("email")}
+                  className="form-control"
+                  id="cp_email"
+                  required
+                  onChange={handleChange}
+                ></input>
+              </div>
+            </div>
+
+
+            <div className='col'>
+
+              <div className="col-md-4">
+                <label htmlFor="Phone_Number" className="form-label">
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  // {...register("Phone_Number")}
+                  className="form-control"
+                  id="Phone_Number"
+                  placeholder="+88"
+                  required
+                  onChange={handleChange}
+                ></input>
+              </div>
+            </div>
+          </div>
+
+          <h2 className="text-center">Sale's Person Information</h2>
+
+          <div className="col">
+            <label htmlFor="SalesPerson" className="form-label">
+              SalesPerson
+            </label>
+            <select className="form-select " onChange={handleChange} id="salesPersonName">
+              <option value="person 1">
+                Person 1
+              </option>
+
+              <option value="Person 2">
+                Person 2
+              </option>
+
+              <option value="Person 3">
+                Person 3
+              </option>
+            </select>
+          </div>
+
+
+          <div className="col-md-6">
             <label htmlFor="firstName" className="form-label">
-              Tag
+            Referred By:
             </label>
             <input
               type="text"
               // {...register("Organization")}
               className="form-control"
-              id="Organization"
+              id="Referred_by"
               required
               onChange={handleChange}
             ></input>
           </div>
 
 
-          <div>
-            <h4>schedule Activity :</h4>
-            <select className="form-select" onChange={handleChange} id="alterBefore">
-              <option value="10">
-                Alert before 10 mins
-              </option>
-
-              <option value="30">
-                Alert before 30 mins
-              </option>
-
-              <option value="60">
-                Alert before 60 mins
-              </option>
-            </select>
-          </div>
-
-
-          {/* <DropDown 
-          handleChange = {handleChange}
-          dropdownlist = {dropDownItems} 
-          heading = "schedule Activity :"
-          >
-
-          </DropDown> */}
-
-
-
-          <div>
-            <input className="m-5"
-              type="time"
-              onChange={handleChange}
-              required 
-            ></input>
-            <input onChange={handleChange} id="date" type="date" required />
-          </div>
-
-
-
-
-          <div className="input-group">
-            <span className="input-group-text">Notes</span>
-            <textarea id="notes" onChange={handleChange} className="form-control" aria-label="With textarea"></textarea>
-          </div>
 
 
 
